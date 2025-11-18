fa9228154f464ce9bbe440d637c224809cf92d0b
Muhamed Husseny <muhammedhusseny@gmail.com>
2025-11-18 18:27:07 +0200

    Add project files.

---
+++ E-Learning.APIs/Program.cs
@@ -0,0 +1,31 @@
+#region Configure Service
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#endregion
+
+
+var app = builder.Build();
+
+#region Configure Kestrel Middlewares
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers(); 
+#endregion
+
+app.Run();
