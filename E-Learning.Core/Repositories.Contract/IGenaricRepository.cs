8c4081896f4dc6e376f70a45a1cbbdce6b032e53
Saad-Magdy <saadeldin.magdy1@gmail.com>
2025-11-19 13:44:25 +0200

    Refactor

--- E-Learning.Core/Repositories.Contract/IGenaricRepository.cs
+++ E-Learning.Core/Repositories.Contract/IGenaricRepository.cs
@@ -1,11 +1,4 @@
-﻿using E_Learning.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace E_Learning.Core.Repositories.Contract
+﻿namespace E_Learning.Core.Repositories.Contract
 {
     public interface IGenaricRepository<T> where T : BaseEntity
     {
