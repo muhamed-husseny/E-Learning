e3cbc937c5eb4cd261e516f1783b4f7367f4d9a6
Muhamed Husseny <muhammedhusseny@gmail.com>
2025-11-19 14:34:19 +0200

    DbContext

--- E-Learning.APIs/Program.cs
+++ E-Learning.APIs/Program.cs
@@ -1,4 +1,7 @@
 #region Configure Service
+using E_Learning.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 #endregion
 
 
