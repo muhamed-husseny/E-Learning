6d05740a9e5098ca05e482672612cee4a83bc640
Saad-Magdy <saadeldin.magdy1@gmail.com>
2025-11-19 15:56:40 +0200

    Refactor GolobalUsing

--- E-Learning.Repository/Data/Configration/StudentConfigration.cs
+++ E-Learning.Repository/Data/Configration/StudentConfigration.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace E_Learning.Repository.Data.Configration
+﻿namespace E_Learning.Repository.Data.Configration
 {
     internal class StudentConfigration : IEntityTypeConfiguration<Student>
     {
